000b12188b5d9e24cece27bebf82fa7fab2d51e2
Sanhil143 <sanhilrai143@gmail.com>
2024-02-12 00:58:15 +0530

    length,distance,normalize,axishelper

--- Transform-Objects/src/script.js
+++ Transform-Objects/src/script.js
@@ -10,11 +10,14 @@ const camera = new Three.PerspectiveCamera(75, window.innerWidth/window.innerHei
 const cubeGeometry = new Three.BoxGeometry(1,1,1);
 const cubeMaterial = new Three.MeshBasicMaterial({color:'white'});
 const cube = new Three.Mesh(cubeGeometry,cubeMaterial);
-cube.position.x = -5
-cube.position.y = -5
-cube.position.z = -5
+cube.position.x = 1
+cube.position.y = -3
+cube.position.z = 3
 console.log(cube.position.length());  //return  orgin ----distance---- cube
 scene.add(cube);
+
+// cube.position.normalize();
+console.log(cube.position.length());  //return  orgin ----distance---- cube
 
 
 const sphereGeometry = new Three.SphereGeometry(1,10,10);
@@ -23,6 +26,13 @@ const sphere =  new Three.Mesh(sphereGeometry,sphereMaterial);
 sphere.position.x = 10
 scene.add(sphere)
 console.log(sphere.position.distanceTo(cube.position)); //return cube and sphere distance
+
+const axisHelper = new Three.AxesHelper(5);
+axisHelper.position.copy(cube.position)
+scene.add(axisHelper)
+
+
+
 camera.position.z = 15;
 
 renderer.render(scene,camera)
